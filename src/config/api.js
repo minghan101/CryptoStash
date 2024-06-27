2cec8832b60f6565dc6f9abe02eb7293f567cd60
brianchanshiyuan <brianchan2002@gmail.com>
2024-06-27 17:07:34 +0800

    news api 1

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
 const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
-const API_KEY = "YOUR-API-KEY";
+const API_KEY = "281af417d8a25cd313500722220619e8";
 export const endpointPath = (country, category) =>
   `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
 export const endpointSearch = (searchQuery) =>
--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-export const navbarBrand = "Your News";
+export const navbarBrand = "CryptoStash News";
 export const header = (category) => `News - Top ${category} Headlines`;
 export const noFound = "No Results Found";
 export const searching = "Searching...";
@@ -10,7 +10,7 @@ export const navs = [
   { nav: "Home", page: "/" },
   { nav: "General", page: "/categories/general" },
   { nav: "Business", page: "/categories/business" },
-  { nav: "Sports", page: "/categories/sports" },
+  { nav: "Crypto", page: "/categories/Crypto" },
   { nav: "Science", page: "/categories/science" },
   { nav: "Health", page: "/categories/health" },
   { nav: "Entertainment", page: "/categories/entertainment" },
@@ -21,14 +21,14 @@ export const router = [
   { path: "/", key: "general", category: "general", country: "us" },
   { path: "/categories/general", key: "general", category: "general", country: "us" },
   { path: "/categories/business", key: "business", category: "business", country: "us" },
-  { path: "/categories/sports", key: "sports", category: "sports", country: "us" },
+  { path: "/categories/Crypto", key: "Crypto", category: "Crypto", country: "us" },
   { path: "/categories/science", key: "science", category: "science", country: "us" },
   { path: "/categories/health", key: "health", category: "health", country: "us" },
   { path: "/categories/entertainment", key: "entertainment", category: "entertainment",country: "us" },
   { path: "/categories/technology", key: "technology", category: "technology", country: "us" }
 ];
 
-export const summary = "Channel and PublishedAt";
+export const summary = "Channel and Published at";
 export const newsChannel = (channel) => `${channel}`;
 export const lastUpdate = (published) =>
   `${moment(published).format("ddd, DD MMM YYYY HH:mm:ss")}`;
